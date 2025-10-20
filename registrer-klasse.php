5eb9386881f3e67c55ae8d46da01bd49ee52eac6
fexento <fredrikbhansen30@gmail.com>
2025-10-20 14:53:29 +0200

    s

---
+++ registrer-klasse.php
@@ -0,0 +1,47 @@
+<?php
+?> 
+
+<h3>Registrer klasse</h3>
+
+<form method="post" action="" id="registrerKlasseSkjema" name="registrerKlasseSkjema">
+  Klassekode <input type="text" id="klassekode" name="klassenavn" required /> <br/>
+  Klassenavn <input type="text" id="klassenavn" name="klassenavn" required /> <br/>
+  Studiumkode <input type="text" id="studiumkode" name="studiumkode" required /> <br/>
+  <input type="submit" value="Registrer klasse" id="registrerKlasseKnapp" name="registrerKlasseKnapp"/> 
+  <input type="reset" value="Nullstill" id="nullstill" name="nullstill" /> <br />
+</form>
+
+<?php 
+  if (isset($_POST ["registrerKlasseKnapp"]))
+    {
+      $klassekode=$_POST ["klassekode"];
+      $klassenavn=$_POST ["klassenavn"];
+      $studiumkode=$_POST ["studiumkode"];
+
+      if (!$klassekode || !$klassenavn  || !$studiumkode)
+        {
+          print "Klassekode, klassenavn og studiumkode må fylles ut.";
+        }
+      else
+        {
+          include("db-tilkobling.php");  /* tilkobling til database-serveren utført og valg av database foretatt */
+
+          $sqlSetning="SELECT * FROM klassekode WHERE postnr='$klassekode';";
+          $sqlResultat=mysqli_query($db,$sqlSetning) or die ("ikke mulig &aring; hente data fra databasen");
+          $antallRader=mysqli_num_rows($sqlResultat); 
+
+          if ($antallRader!=0)  /* poststedet er registrert fra før */
+            {
+              print ("Poststedet er registrert fra før");
+            }
+          else
+            {
+              $sqlSetning="INSERT INTO klassekode VALUES('$klassekode','$klassenavn');";
+              mysqli_query($db,$sqlSetning) or die ("ikke mulig &aring; registrere data i databasen");
+                /* SQL-setning sendt til database-serveren */
+
+              print ("F&oslash;lgende poststed er n&aring; registrert: $postnr $poststed"); 
+            }
+        }
+    }
+?> 
