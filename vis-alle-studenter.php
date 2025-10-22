8f796906cf982357047a9beebaac5be1d715d8eb
fexento <fredrikbhansen30@gmail.com>
2025-10-22 14:35:51 +0200

    s

---
+++ vis-alle-studenter.php
@@ -0,0 +1,26 @@
+<?php 
+  include("db-tilkobling.php");  /* tilkobling til database-serveren utf�rt og valg av database foretatt */
+
+  $sqlSetning="SELECT * FROM student;";
+  
+  $sqlResultat=mysqli_query($db,$sqlSetning) or die ("ikke mulig &aring; hente data fra databasen");
+    /* SQL-setning sendt til database-serveren */
+	
+  $antallRader=mysqli_num_rows($sqlResultat);  /* antall rader i resultatet beregnet */
+
+  print ("<h3>Registrerte studenter</h3>");
+  print ("<table border=1>");  
+  print ("<tr><th align=left>brukernavn</th> <th align=left>fornavn</th><th align=left>etternavn</th><th align=left>klassekode</th></tr>"); 
+
+  for ($r=1;$r<=$antallRader;$r++)
+    {
+      $rad=mysqli_fetch_array($sqlResultat);  /* ny rad hentet fra sp�rringsresultatet */
+      $brukernavn=$rad["postnr"];       
+      $fornavn=$rad["poststed"];
+      $etternavn=$rad["etternavn"];
+      $klassekode=$rad["klassekode"];    
+
+      print ("<tr> <td> $brukernavn </td> <td> $fornavn </td> <td> $etternavn </td> <td> klassekode </td> </tr>");
+    }
+  print ("</table>"); 
+?>
