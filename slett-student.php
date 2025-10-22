02ba2ce09bddb67d12e02fda0274961a44e3038f
fexento <fredrikbhansen30@gmail.com>
2025-10-22 13:57:43 +0200

    s

---
+++ slett-student.php
@@ -0,0 +1,44 @@
+<?php  
+?> 
+
+<script src="funksjoner.js"> </script>
+
+<h3>Slett student</h3>
+
+<form method="post" action="" id="slettStudentSkjema" name="slettStudentSkjema" onSubmit="return bekreft()">
+  Brukernavn <input type="text" id="brukernavn" name="brukernavn" required /> <br/>
+  <input type="submit" value="Slett student" name="slettStudentKnapp" id="slettStudentKnapp" /> 
+</form>
+
+<?php
+  if (isset($_POST ["slettStudentKnapp"]))
+    {	
+      $brukernavn=$_POST ["brukernavn"];
+	  
+      if (!$brukernavn)
+        {
+          print ("Brukernavn må fylles ut");
+        }
+      else
+        {
+          include("db-tilkobling.php");  /* tilkobling til database-serveren utført og valg av database foretatt */
+
+          $sqlSetning="SELECT * FROM student WHERE brukernavn='$brukernavn';";
+          $sqlResultat=mysqli_query($db,$sqlSetning) or die ("ikke mulig &aring; hente data fra databasen");
+          $antallRader=mysqli_num_rows($sqlResultat); 
+
+          if ($antallRader==0)  /* poststedet er ikke registrert */
+            {
+              print ("Studenten finnes ikke");
+            }
+          else
+            {	  
+              $sqlSetning="DELETE FROM student WHERE brukernavn='$brukernavn';";
+              mysqli_query($db,$sqlSetning) or die ("ikke mulig &aring; slette data i databasen");
+                /* SQL-setning sendt til database-serveren */
+		
+              print ("F&oslash;lgende student er nå slettet: $brukernavn  <br />");
+            }
+        }
+    }
+?> 
